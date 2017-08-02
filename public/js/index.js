5708154aeee5d67ca3f0c79a9821b2b880b061d1
Charles Lee <charleslee90@gmail.com>
2017-08-02 10:44:34 +0800

    Updates dependencies

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,51 @@
+
+import '../css/styles.css';
+
+/**
+ * Utility Functions
+ */
+
+function removeAllChildren(parent) {
+  while (parent.hasChildNodes()) {
+    parent.removeChild(parent.lastChild);
+  }
+}
+
+function findParent(node, className) {
+  let tempNode = node;
+
+  while (!tempNode.classList.contains(className)) {
+    tempNode = tempNode.parentNode;
+  }
+
+  return tempNode
+}
+
+function sliceArray(nodeArray) {
+  return Array.prototype.slice.call(nodeArray)
+}
+
+function getDistanceFromLeft(el) {
+  const rect = el.getBoundingClientRect();
+  const docEl = document.documentElement;
+
+  return (rect.left + (window.pageXOffset || docEl.scrollLeft || 0))
+}
+
+function scrollTop() {
+  return (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
+}
+
+function scrollTo(e, elem) {
+  e.preventDefault();
+
+  const target = `.${elem.getAttribute('scrollTo')}`
+
+  $('html, body').animate({
+    scrollTop: $(target).offset().top
+  }, 1000);
+}
+
 window.addEventListener('load', function(e) {
   console.log('--> Application started')
 })
