c18ea2a116c06f06d7033ea901e3492f608945b1
Charles Lee <charleslee90@gmail.com>
2017-08-30 17:44:20 +0800

    New full page structure

--- public/js/home.js
+++ public/js/home.js
@@ -4,10 +4,33 @@ import { hotReload } from './util.js'
 
 window.addEventListener('load', function(e) {
   console.log('--> Application started  |  Current route: /')
+  const patternOne = document.querySelector('#patternOne');
+  const patternTwo = document.querySelector('#patternTwo');
+  const patternThree = document.querySelector('#patternThree');
+  const patternFour = document.querySelector('#patternFour');
+
+  $('#fullpage').fullpage({
+    afterLoad: function(anchorLink, index) {
+      if (index === 1) {
+        patternOne.classList.add('animate');
+        patternTwo.classList.add('animate');
+      } else if (index === 2) {
+        patternThree.classList.add('animate');
+      } else if (index === 3) {
+        patternFour.classList.add('animate');
+      }
+    },
+
+    afterRender() {
+      patternOne.classList.add('animate');
+      patternTwo.classList.add('animate');
+    }
+  })
+
+  window.start = function() {
+  }
+
 })
 
-window.start = function() {
-
-}
 
 hotReload();
