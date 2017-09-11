477a31168767c2a64008e2b2c0f74973c9a34475
Charles Lee <charleslee90@gmail.com>
2017-09-11 21:43:24 +0800

    Intro page basic animations done

--- public/js/home.js
+++ public/js/home.js
@@ -2,24 +2,28 @@
 import '../css/home.css';
 import { hotReload } from './util.js'
 
+const patternOne = document.querySelector('#patternOne');
+const patternTwo = document.querySelector('#patternTwo');
+const patternThree = document.querySelector('#patternThree');
+const patternFour = document.querySelector('#patternFour');
+const logoImage = document.querySelector('.logo-image');
+const splashHeading = document.querySelector('.splashHeading');
+
 window.addEventListener('load', function(e) {
   console.log('--> Application started  |  Current route: /')
+  patternOne.classList.add('animate');
+  patternTwo.classList.add('animate');
+  logoImage.classList.add('animate');
+  splashHeading.classList.add('animate');
 })
 
 $(function() {
-  const patternOne = document.querySelector('#patternOne');
-  const patternTwo = document.querySelector('#patternTwo');
-  const patternThree = document.querySelector('#patternThree');
-  const patternFour = document.querySelector('#patternFour');
 
   $('#fullpage').fullpage({
     scrollingSpeed: 1000,
 
     afterLoad: function(anchorLink, index) {
-      if (index === 1) {
-        // patternOne.classList.add('animate');
-        // patternTwo.classList.add('animate');
-      } else if (index === 2) {
+      if (index === 2) {
         patternThree.classList.add('animate');
       } else if (index === 3) {
         patternFour.classList.add('animate');
